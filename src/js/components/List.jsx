3a13e49e326b6ef3f1ab3cdd365ad8a5a02f19d5
Samuel Piedra <114516225+SPiedra955@users.noreply.github.com>
2026-03-22 23:06:43 +0000

    adding tasks to the list

---
+++ src/js/components/List.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+
+const List = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    return (
+        <div className=" container-fluid w-50">
+            <div>
+                <h1 className="text-center mt-5 text-danger display-1">TODOS</h1>
+            </div>
+            <div className="container ">
+                <ul className="list-group list-group-flush border mt-2 d-flex justify-content-between">
+                    <li className="list-group-item">
+                        <input
+                            className="w-100 border-0"
+                            type="text border-0"
+                            placeholder="What do you need to do ?"
+                            onChange={(e) => setInputValue(e.target.value)}
+                            value={inputValue}
+                            onKeyUpCapture={(e) => e.key === 'Enter' ? setTodos([...todos, inputValue]) : []}
+                        >
+                        </input>
+                    </li>
+                    {todos.map((el, i) =>
+                        <li className="list-group-item d-flex justify-content-between" key={i}>
+                            <span>{el}</span> <FontAwesomeIcon icon={faX} />
+                        </li>)}
+                </ul>
+                <div className="py-2">
+                    <p>{todos.length > 0 ? `${todos.length} items` : 'No hay tareas, añadir tareas'}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default List;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,10 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import List from './components/List';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <List/>
   </React.StrictMode>,
 )
